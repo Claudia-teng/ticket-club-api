c1d8b62bd0037ae5052551cb0d707a12b6cf54fa
Claudia-teng <sydeng102@gmail.com>
2022-08-31 14:43:25 +0800

    feat: update data schema dump file

--- dump.sql
+++ dump.sql
@@ -33,16 +33,6 @@ CREATE TABLE `area` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `area`
---
-
-LOCK TABLES `area` WRITE;
-/*!40000 ALTER TABLE `area` DISABLE KEYS */;
-INSERT INTO `area` VALUES (1,1,'1樓A區');
-/*!40000 ALTER TABLE `area` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `event`
 --
 
@@ -59,16 +49,6 @@ CREATE TABLE `event` (
   FULLTEXT KEY `FULLTEXT` (`description`,`title`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `event`
---
-
-LOCK TABLES `event` WRITE;
-/*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES (1,'LANY a november to remember','LANY','後疫情時代迫不及待要用LIVE舞台和全球歌迷見面的加州新生代樂團LANY，兩位高顏質成員Paul Klein和Jake Goss繼2017年首度在台開唱之後，將二度以浪漫搖滾風潮席捲台灣。累積超過35億線上串流點聽次數、7.5億的影片點閱次數和全球逾50萬的演唱會動員人數。LANY為亞洲歌迷專屬打造最難忘的11月全新巡演，疫情期間發行的兩張新專輯【Mama’s Boy】、【gg bb xx】收錄歌曲一次完美呈現，從第一站台灣起跑，帶給歌迷亞洲限定的全新音樂感受和現場體驗！','1657956850051.png');
-/*!40000 ALTER TABLE `event` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `order`
@@ -92,16 +72,6 @@ CREATE TABLE `order` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,1,1,'1',3000);
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `order_detail`
 --
 
@@ -122,16 +92,6 @@ CREATE TABLE `order_detail` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order_detail`
---
-
-LOCK TABLES `order_detail` WRITE;
-/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
-INSERT INTO `order_detail` VALUES (1,1,1,3000);
-/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `price`
 --
 
@@ -142,28 +102,14 @@ CREATE TABLE `price` (
   `id` int NOT NULL AUTO_INCREMENT,
   `session_id` int NOT NULL,
   `area_id` int NOT NULL,
-  `seat_id` int NOT NULL,
   `price` int NOT NULL,
-  `status_id` char(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_price_session_idx` (`session_id`),
   KEY `fk_price_area_idx` (`area_id`),
-  KEY `fk_price_seat_idx` (`seat_id`),
   CONSTRAINT `fk_price_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`),
-  CONSTRAINT `fk_price_seat` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`id`),
   CONSTRAINT `fk_price_session` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `price`
---
-
-LOCK TABLES `price` WRITE;
-/*!40000 ALTER TABLE `price` DISABLE KEYS */;
-INSERT INTO `price` VALUES (1,1,1,1,3000,'1');
-/*!40000 ALTER TABLE `price` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `seat`
@@ -174,27 +120,14 @@ DROP TABLE IF EXISTS `seat`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `seat` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `venue_id` int NOT NULL,
   `area_id` int NOT NULL,
   `column` int NOT NULL,
   `row` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_seat_venue_idx` (`venue_id`),
   KEY `fk_seat_area_idx` (`area_id`),
-  CONSTRAINT `fk_seat_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_seat_venue` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_seat_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `seat`
---
-
-LOCK TABLES `seat` WRITE;
-/*!40000 ALTER TABLE `seat` DISABLE KEYS */;
-INSERT INTO `seat` VALUES (1,1,1,1,1);
-/*!40000 ALTER TABLE `seat` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `session`
@@ -218,16 +151,6 @@ CREATE TABLE `session` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `session`
---
-
-LOCK TABLES `session` WRITE;
-/*!40000 ALTER TABLE `session` DISABLE KEYS */;
-INSERT INTO `session` VALUES (1,1,'2022-10-31 20:00:00',1,'1657956850051.png');
-/*!40000 ALTER TABLE `session` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -245,16 +168,6 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'User1','user1@gmail.com','123');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `venue`
 --
 
@@ -268,16 +181,6 @@ CREATE TABLE `venue` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `venue`
---
-
-LOCK TABLES `venue` WRITE;
-/*!40000 ALTER TABLE `venue` DISABLE KEYS */;
-INSERT INTO `venue` VALUES (1,'Taipei','Zepp New Taipei');
-/*!40000 ALTER TABLE `venue` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -288,4 +191,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-31 13:52:22
+-- Dump completed on 2022-08-31 14:42:29
