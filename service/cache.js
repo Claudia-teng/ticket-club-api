be1a0c27666e23cffa1951f13e52c409357930cb
Claudia-teng <sydeng102@gmail.com>
2022-09-15 13:14:58 +0800

    feat: update Dockerfile

--- service/cache.js
+++ service/cache.js
@@ -1,12 +1,6 @@
 require('dotenv').config();
 const Redis = require('ioredis');
-const redis = new Redis({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST,
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  tls: {},
-});
+const redis = new Redis();
 
 redis.on('connect', () => {
   console.log('Redis connected!');
