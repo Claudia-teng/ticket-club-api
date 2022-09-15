a3a350e8ddc1cad195c743d3f28777f7b41b41cd
Claudia-teng <sydeng102@gmail.com>
2022-09-15 16:29:34 +0800

    feat: remove command port

--- service/cache.js
+++ service/cache.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const Redis = require('ioredis');
-const redis = new Redis();
+const redis = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  tls: {},
+});
 
 redis.on('connect', () => {
   console.log('Redis connected!');
